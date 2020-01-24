f4352210bdce7dca4465ff88bc899ee94d3e3c3a
DairoArley <dagavergara@gmail.com>
2020-01-24 07:26:05 -0500

    Se actualizan las versiones de las dependencias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,26 @@
-import React from'react';
+import React, {useState} from'react';
+import {Container, Typography, Card, Grid, TextField, Icon} from '@material-ui/core';
 
-export default () => (<div>Home</div>)
+export default () => {
+	const [serchText, setSerchText] = useState('');
+
+	const handleSearchTextChange = event => {
+		setSerchText(event.target.value);
+	};
+	return (
+		<Container>
+			<Card>
+				<Grid container>
+					<Grid>
+						<Typography>Bienvenidos</Typography>
+					</Grid>
+					<Icon></Icon>
+					<Grid>
+						<Typography>Icon</Typography>
+					</Grid>
+				</Grid>
+				<TextField value={serchText} placeholder="Buscar..." onChange={handleSearchTextChange}/>
+			</Card>
+		</Container>		
+	);
+}
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,4 @@
 import { all } from 'redux-saga/effects';
-import watchDemo from './watcher';
 
 export default function* rootSaga() {
 	yield all([]);
