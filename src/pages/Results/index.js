eabfb6a46751735cfa99225bff117fc3f5f4d1a5
DairoArley <dagavergara@gmail.com>
2020-01-31 22:38:27 -0500

    se crea el programa totalmente funcional

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,25 +1,39 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {Container} from '@material-ui/core';
+import {Container, CircularProgress  } from '@material-ui/core';
 import queryString from 'query-string';
 
 import {searchMovie } from '../../redux/actions/search';
+import {movieResults, isSearchLoading} from '../../redux/selectors';
+import  MovieResult from '../../components/MovieResult';
 
 export default ({location}) => {
     const dispatch = useDispatch();
     console.log(queryString.parse(location.search));
-    const movies = useSelector( state => {
-        console.log(state);
-    });
+    const movies = useSelector( state => movieResults(state) );
+    const isLoading = useSelector(state => isSearchLoading(state) );
+    const [isLooked, setIsLooked] = useState(false);
 
     useEffect( () => {
         const { movieName } = queryString.parse(location.search);
-        dispatch(searchMovie({movieName}));
+        if(movieName && !movies){
+            dispatch(searchMovie({movieName}));
+        }
+        
     });
+
+    const renderMovies = () => {
+        if (movies) {
+            return movies.map( (value, index )=> <MovieResult key={index} {...value}/>);
+        }else if(isLoading){
+            return <CircularProgress size={100} color="primary"/>
+        }
+        return <div></div>
+    };
 
     return(
         <Container>
-            Resultados
+            {renderMovies()}
         </Container>
     )
 }
--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const keyOMBD ='c36362ed'; 
-
 const baseURL = 'http://www.omdbapi.com/?apikey=c36362ed'
 
 export const apiCall = (url, data, headers, method) => axios({
